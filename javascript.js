f10437f99c42ce1d83dab6fa4df0dba3fbbe6952
Ismail <141173099+alcode111@users.noreply.github.com>
2023-09-28 14:58:42 +0200

    Create a playRound function

--- javascript.js
+++ javascript.js
@@ -8,3 +8,31 @@ function getComputerChoice(){
         return "scissors"
     }
 }
+
+function playRound(){
+    let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+    let computerChoice = getComputerChoice();
+
+    if(playerChoice === computerChoice){
+        console.log(`${playerChoice} VS ${computerChoice}`);
+        console.log("This is a tie.")
+        return "Draw";
+    
+    } else if(playerChoice === "rock" && computerChoice === "scissors" ||
+            playerChoice === "paper" && computerChoice === "rock" ||
+            playerChoice === "scissors" && computerChoice === "paper"){
+                console.log(`${playerChoice} VS ${computerChoice}`);
+                console.log(`${playerChoice} BEATS ${computerChoice}`);
+                return "Win";
+    
+    } else if(playerChoice === "rock" && computerChoice === "paper" ||
+            playerChoice === "paper" && computerChoice === "scissors" ||
+            playerChoice === "scissors" && computerChoice === "rock"){
+                console.log(`${playerChoice} VS ${computerChoice}`);
+                console.log(`${computerChoice} BEATS ${playerChoice}`);
+                return "Defeat";
+    
+    } else {
+        console.log("Please, only select one of the three options.");
+    }
+}
